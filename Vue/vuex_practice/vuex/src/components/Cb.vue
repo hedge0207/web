189896ce49fb5b2b8768786181eb1a56b35b798c
Cha <hedge0207@gmail.com>
2020-07-19 23:51:57 +0900

    0719

---
+++ Vue/vuex_practice/vuex/src/components/Cb.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Cb</h1>
+      <div>{{MFCaTCb}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Cb',
+    data: function(){
+        return {
+            tmp:'',
+        }
+    },
+    props:{
+        MFCaTCb:String,
+    }
+}
+</script>
+
+<style>
+
+</style>
